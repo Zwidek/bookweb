1b3cf862d95b6ea3b584da8dd1cad0085f987be3
Mgr <zwidekxd@gmail.comgit config --global user.email zwidekxd@gmail.comgit config --global user.email zwidekxd@gmail.com>
2022-04-12 22:55:20 +0200

    Dodanie klasy productVM - funkcjonalności: wyświetlanie Category oraz CoverType. Implementacja productVM do ProductController

---
+++ BookWeb.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWeb.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public Product Product = new();
+        public IEnumerable<SelectListItem> CategoryList { get; set; }  
+        public IEnumerable<SelectListItem> CoverTypeList {get; set; }   
+    }
+}
--- BookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using BookWeb.DataAccess;
 using BookWeb.DataAccess.Repository.IRepository;
 using BookWeb.Models;
+using BookWeb.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -23,39 +24,39 @@ namespace BookWeb.Controllers;
         //GET
         public IActionResult Upsert(int? id)
         {
-            Product product = new();
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
-                u => new SelectListItem
+        ProductVM productVM = new()
+        {
+            Product = new(),
+            CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
             {
-                Text = u.Name,
-                Value = u.Id.ToString()
-            });
-
-            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
-                u => new SelectListItem
-                {
-                Text = u.Name,
-                Value = u.Id.ToString()
-                });
+                Text = i.Name,
+                Value = i.Id.ToString()
+            }),
+            CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            }),
+        };
 
             if (id==null && id == 0)
             {
             //create product 
-                ViewBag.CategoryList = CategoryList;
-                ViewData["CoverTypeList"] = CoverTypeList;
-                return View(product);
+            //    ViewBag.CategoryList = CategoryList;
+            //    ViewData["CoverTypeList"] = CoverTypeList;
+                return View(productVM);
             }
             else
             {
             //update
-                ViewBag.CategoryList = CategoryList;
-                ViewData["CoverTypeList"] = CoverTypeList;
-                return View(product);
+             //   ViewBag.CategoryList = CategoryList;
+             //   ViewData["CoverTypeList"] = CoverTypeList;
+                return View(productVM);
 
 
             }
 
-            return View(product);
+            return View(productVM);
 
         }
 
