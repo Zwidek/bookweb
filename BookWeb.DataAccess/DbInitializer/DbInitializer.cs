87d76fdbe621172f6d9dff8e48c576f141a79cba
Mgr <zwidekxd@gmail.comgit config --global user.email zwidekxd@gmail.comgit config --global user.email zwidekxd@gmail.com>
2022-04-25 17:55:45 +0200

    Migracja bazy danych do azure

---
+++ BookWeb.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using BookWeb.Utility;
+using BookWeb.Models;
+
+namespace BookWeb.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext db)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _db = db;
+        }
+
+
+        public void Initialize()
+        {
+            //migracje
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            
+            //tworzenie ról
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Indi)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Comp)).GetAwaiter().GetResult();
+
+                // tworzenie admina
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "marcin.gres.studia@gmail.com",
+                    Email = "marcin.gres.studia@gmail.com",
+                    Name = "Marcin Gres",
+                    PhoneNumber = "1026931997",
+                    StreetAddress = "Wiejska 45A",
+                    State = "Podlaskie",
+                    PostalCode = "15-393",
+                    City = "Bialystok"
+                }, "Test12345!").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@dotnetmastery.com");
+
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+
+            }
+            return;
+        }
+    }
+}
--- BookWeb/Program.cs
+++ BookWeb/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using BookWeb.Utility;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Stripe;
+using BookWeb.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection");;
@@ -21,6 +22,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     ));
 builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.ConfigureApplicationCookie(options =>
@@ -53,7 +55,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
-
+SeedDatabase();
 app.UseAuthentication();
 
 app.UseAuthorization();
@@ -64,3 +66,13 @@ app.MapControllerRoute(
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
